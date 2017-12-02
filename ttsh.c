19869766015f32e1182c6520e881ec9653929b5f
Patrick Hall <patrickhall@sandiego.edu>
2017-12-01 19:08:49 -0800

    i think I fixed it but I don't even know anymore

--- ttsh.c
+++ ttsh.c
@@ -28,21 +28,18 @@ void cd(char *argv[]);
 void unix_error(char*msg);
 pid_t Fork(void); 
 void child_handler(__attribute__ ((unused)) int sig);
+int builtIn(char *argv[]); 
 
 int main() { 
-	// TODO: Add a call to sigaction to register your SIGCHLD signal handler
-	// here. See the write-up for more details on sigaction.
 	struct sigaction sa;
 	sa.sa_handler = child_handler;
 	sa.sa_flags = SA_NOCLDSTOP;
 	sigaction(SIGCHLD, &sa, NULL);
 
 	while(1) {
-		// (1) print the shell prompt
 		fprintf(stdout, "ttsh> ");  
 		fflush(stdout);
 
-		// (2) read in the next command entered by the user
 		char cmdline[MAXLINE];
 		if ((fgets(cmdline, MAXLINE, stdin) == NULL)
 				&& ferror(stdin)) {
@@ -64,12 +61,8 @@ int main() {
 			exit(0);
 		}
 
-		// TODO: remove this line after you are done testing/debugging.
 		//fprintf(stdout, "DEBUG: %s\n", cmdline);
 
-		// TODO: complete top-level steps
-		// (3) make a call to parseArguments function to parse it into its argv
-		// format
 		char *argv[MAXARGS];
 		int ret = parseArguments(cmdline, argv);
 
@@ -78,39 +71,53 @@ int main() {
 		}
 
 		addEntry(cmdline);	
-		// (4) Call a function that will determine how to execute the command
-		// that the user entered, and then execute it
-		execCmd(argv, ret);
+
+		if (builtIn(argv) == 1) {
+			continue;
+		}
+		else {
+			execCmd(argv, ret);
+		}
 	}
 	return 0;
+}
+
+int builtIn(char *argv[]) {
+	if (strcmp(argv[0], "exit") == 0) {
+		fprintf(stdout, "adios...\n");
+		exit(0);
+	}
+	else if (strcmp(argv[0], "history") == 0) {
+		printHistory();
+		return 1;
+	}
+	else {
+		return 0;
+	}
 }
 
 /*
  * Execute commands in argv
  */
 void execCmd(char *argv[], int ret) {
-	if (strcmp(argv[0], "exit") == 0) {
-		fprintf(stdout, "adios...\n");
-		exit(0);
-	}
-	else if(strcmp(argv[0], "history") == 0) {
-		printHistory();
-		return;
-	}
-	else {
-		int status;
-		pid_t child_pid; 		
-		if ((child_pid = Fork()) == 0) { // Child
-			if (execvp(argv[0], argv) == -1) {
-				fprintf(stdout, "command no es aqui\n");
-				exit(0);
-			}
+	int status;
+	pid_t child_pid; 		
+	if ((child_pid = Fork()) == 0) { // Child runs this
+		setpgid(0, 0);
+		if (execvp(argv[0], argv) == -1) {
+			fprintf(stdout, "command no es aqui\n");
+			exit(0);
 		}
-		else {
-			if (ret == 0) { // background
-				waitpid(child_pid, &status, 0);
-			}
+	}
+	else { // Parent runs this
+		if (ret == 0) { //  foregrund
+			fprintf(stdout, "\t\t foreground\n");
+			waitpid(child_pid, &status, 0); // wait for child
 			return;
+		}
+		else { //background
+			fprintf(stdout, "\t\t background\n");
+			return; //dont wait for child.
 		}
 	}
 }
@@ -158,7 +165,7 @@ pid_t Fork(void) {
 void child_handler(__attribute__ ((unused)) int sig) {
 	pid_t pid;
 	int status;
-	while((pid = waitpid(pid, &status, 0)) != -1) {
-		//handle death of pid, pid
+	while((pid = waitpid(pid, &status, WNOHANG)) != -1) {
+		// chill here
 	}
 }
